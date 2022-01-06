b1ed856e06ff4807ebf8e66558192c63484f0ed9
SeaIceNX <seaice233@outlook.com>
2022-01-06 14:28:16 +0800

    fix

--- Clouddream.Utils/Logger.cs
+++ Clouddream.Utils/Logger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 
@@ -14,39 +15,39 @@ namespace Clouddream.Utils
             ERROR = 3,
             FATAL = 4
         }
-
         public enum LogType
         {
             OnlyConsole = 0,
             OnlyLogFile = 1,
             ConsoleWithLogFile = 2
         }
-
         public static void Trace(string message, LogLevel logLevel = LogLevel.INFO, LogType logType = LogType.OnlyConsole, string logFilePath = "")
         {
 #if !DEBUG
             if (logLevel != LogLevel.DEBUG)
             {
 #endif
-            switch (logType)
-            {
-                case LogType.OnlyConsole:
-                    {
-                        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss} {logLevel}] [{Assembly.GetCallingAssembly().GetName().Name} - {MethodBase.GetCurrentMethod().Name}] {message}");
-                        break;
-                    }
-                case LogType.OnlyLogFile:
-                    {
-                        File.AppendAllLines(string.IsNullOrWhiteSpace(logFilePath) ? $"{DateTime.Now:yyyy-MM-dd}" : logFilePath, new[] { $"[{DateTime.Now} {logLevel}] [{Assembly.GetCallingAssembly().GetName().Name} - {MethodBase.GetCurrentMethod().Name}] {message}" });
-                        break;
-                    }
-                case LogType.ConsoleWithLogFile:
-                    {
-                        Console.WriteLine($"[{DateTime.Now:yyyy - MM - dd HH:mm:ss} {logLevel}] {message}");
-                        File.AppendAllLines(string.IsNullOrWhiteSpace(logFilePath) ? $"{DateTime.Now:yyyy-MM-dd}" : logFilePath, new[] { $"[{DateTime.Now} {logLevel}] [{Assembly.GetCallingAssembly().GetName().Name} - {MethodBase.GetCurrentMethod().Name}] {message}" });
-                        break;
-                    }
-            }
+                string methodName = new StackTrace().GetFrame(1).GetMethod().Name;
+                string assemblyName = Assembly.GetCallingAssembly().GetName().Name;
+                switch (logType)
+                {
+                    case LogType.OnlyConsole:
+                        {
+                            Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss} {logLevel}] [{assemblyName} - {methodName}] {message}");
+                            break;
+                        }
+                    case LogType.OnlyLogFile:
+                        {
+                            File.AppendAllLines(string.IsNullOrWhiteSpace(logFilePath) ? $"{DateTime.Now:yyyy-MM-dd}" : logFilePath, new[] { $"[{DateTime.Now} {logLevel}] [{assemblyName} - {methodName}] {message}" });
+                            break;
+                        }
+                    case LogType.ConsoleWithLogFile:
+                        {
+                            Console.WriteLine($"[{DateTime.Now:yyyy - MM - dd HH:mm:ss} {logLevel}] {message}");
+                            File.AppendAllLines(string.IsNullOrWhiteSpace(logFilePath) ? $"{DateTime.Now:yyyy-MM-dd}" : logFilePath, new[] { $"[{DateTime.Now} {logLevel}] [{assemblyName} - {methodName}] {message}" });
+                            break;
+                        }
+                }
 #if !DEBUG
             }
 #endif
